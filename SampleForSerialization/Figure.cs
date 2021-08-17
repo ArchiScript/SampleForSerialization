88024516fa6c384dacacea2292db9b75b45a0001
Артур <arturskomarohov@gmail.com>
2021-08-17 21:59:58 +0300

    dictionary serialization try

--- SampleForSerialization/Figure.cs
+++ SampleForSerialization/Figure.cs
@@ -6,8 +6,10 @@ namespace SampleForSerialization
 {
     [Serializable]
     class Figure
-    {public string Name { get; set; }
+    {
+        public string Name { get; set; }
         public int SideCount { get; set; }
         public double SideLength { get; set; }
+        public Figure Figure1 { get; set; }
     }
 }
--- SampleForSerialization/Program.cs
+++ SampleForSerialization/Program.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 
+
 namespace SampleForSerialization
 {
     class Program
@@ -14,8 +15,43 @@ namespace SampleForSerialization
                 SideCount = 3,
                 SideLength = 2
             };
-            var serTriangle = JsonConvert.SerializeObject(triangle);
+            var serTriangle = JsonConvert.SerializeObject(triangle, Formatting.Indented);
             List<Figure> figList = new List<Figure>() { triangle, triangle, triangle };
+            var serListTriangle = JsonConvert.SerializeObject(figList, Formatting.Indented);
+            // Console.WriteLine(serTriangle);
+            //Console.WriteLine(serListTriangle);
+            Dictionary<string, Figure> figDic = new Dictionary<string, Figure>();
+            for (int i = 1; i < 5; i++)
+            {
+                figDic.Add($"Треугольник{i}", new Figure
+                {
+                    Name = $"Name{i}",
+                    SideCount = 2,
+                    SideLength = 3,
+                    Figure1 = new Figure
+                    {
+                        Name = $"SubName{i}",
+                        SideCount = 3,
+                        SideLength = 8,
+                        Figure1 = new Figure
+                        {
+                            Name = $"SUBSubName{i}",
+                            SideLength = 3,
+                            SideCount = 88
+                        }
+                    }
+                });
+            }
+
+            var serDic = JsonConvert.SerializeObject(figDic, Formatting.Indented);
+            Console.WriteLine(serDic);
+            Dictionary<string, int> dic2 = new Dictionary<string, int>();
+            for (int i = 0; i < 5; i++)
+            {
+                dic2.Add($"dklfjj{i}", i);
+            }
+            string dic2ser = JsonConvert.SerializeObject(dic2.Keys, Formatting.Indented);
+            Console.WriteLine(dic2ser);
         }
     }
 }
