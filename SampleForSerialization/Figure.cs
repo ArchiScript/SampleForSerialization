f51727750a5bcf4877868cc958c76a973b1fad6b
Артур <arturskomarohov@gmail.com>
2021-08-18 10:54:46 +0300

    obj list dict serialize methods

--- SampleForSerialization/Figure.cs
+++ SampleForSerialization/Figure.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace SampleForSerialization
 {
@@ -11,5 +12,41 @@ namespace SampleForSerialization
         public int SideCount { get; set; }
         public double SideLength { get; set; }
         public Figure Figure1 { get; set; }
+
+
+
+        public static string SerializeObjToJson(Figure figure)
+        {
+            string strObj = JsonConvert.SerializeObject(figure, Formatting.Indented);
+            return strObj;
+        }
+        public static string SerializeListToJson(List<Figure> figList)
+        {
+            string strListOfObj = JsonConvert.SerializeObject(figList, Formatting.Indented);
+            return strListOfObj;
+        }
+        public static string SerializeDictToJson(Dictionary<string, Figure> figDic)
+        {
+            string strDict = JsonConvert.SerializeObject(figDic, Formatting.Indented);
+            return strDict;
+        }
+        public static Figure DeserializeFromJson(string strJson)
+        {
+            var figObj = JsonConvert.DeserializeObject<Figure>(strJson);
+            return figObj;
+        }
+        public static List<Figure> DeserializeListFromJson(string listJson)
+        {
+            var listObj = JsonConvert.DeserializeObject<List<Figure>>(listJson);
+            return listObj;
+        }
+        public static Dictionary<string, Figure>DeserializeDicFromJson(string dicJson)
+        {
+            var dicObj = JsonConvert.DeserializeObject<Dictionary<string, Figure>>(dicJson);
+            return dicObj;
+        }
     }
+
+
+   
 }
--- SampleForSerialization/Program.cs
+++ SampleForSerialization/Program.cs
@@ -13,14 +13,15 @@ namespace SampleForSerialization
             {
                 Name = "Triangle",
                 SideCount = 3,
-                SideLength = 2
+                SideLength = 2,
+                
+           
             };
-            var serTriangle = JsonConvert.SerializeObject(triangle, Formatting.Indented);
+            
             List<Figure> figList = new List<Figure>() { triangle, triangle, triangle };
-            var serListTriangle = JsonConvert.SerializeObject(figList, Formatting.Indented);
-            // Console.WriteLine(serTriangle);
-            //Console.WriteLine(serListTriangle);
+                
             Dictionary<string, Figure> figDic = new Dictionary<string, Figure>();
+           
             for (int i = 1; i < 5; i++)
             {
                 figDic.Add($"Треугольник{i}", new Figure
@@ -44,14 +45,48 @@ namespace SampleForSerialization
             }
 
             var serDic = JsonConvert.SerializeObject(figDic, Formatting.Indented);
-            Console.WriteLine(serDic);
+            //Console.WriteLine(serDic);
             Dictionary<string, int> dic2 = new Dictionary<string, int>();
             for (int i = 0; i < 5; i++)
             {
                 dic2.Add($"dklfjj{i}", i);
             }
             string dic2ser = JsonConvert.SerializeObject(dic2.Keys, Formatting.Indented);
-            Console.WriteLine(dic2ser);
+
+
+            var fig = new Figure();
+
+            var jsonFromObj = Figure.SerializeObjToJson(triangle);
+            var objFromJson = Figure.DeserializeFromJson(jsonFromObj);
+            var jsonFromList = Figure.SerializeListToJson(figList);
+            var jsonFromDic = Figure.SerializeDictToJson(figDic);
+            var listFromJson = Figure.DeserializeListFromJson(jsonFromList);
+            var dicFromJson = Figure.DeserializeDicFromJson(jsonFromDic);
+            Console.WriteLine($"Это возврат метода SerializeObj: {jsonFromObj}");
+            Console.WriteLine($"Это возврат метода SerializeList:{jsonFromList}");
+            Console.WriteLine($"Это возврат метода SerializeDict: (вложенный)  {jsonFromDic}");
+            Console.WriteLine($"Это возврат метода DeserializeObjFromJson: {objFromJson.Name} {objFromJson.SideCount}" +
+                $" {objFromJson.SideLength}{objFromJson.Figure1} \n");
+
+            Console.WriteLine($"Это возврат метода DeserializeListFromJson: \n");
+            foreach (var item in listFromJson)
+            {
+                Console.WriteLine($" {item.Name}{item.SideCount}{item.SideLength}");
+            }
+
+            Console.WriteLine($"Это возврат метода DeserializeDicFromJson  \n");
+            foreach (var item in dicFromJson)
+            {
+                Console.WriteLine($"{item.Key}");
+                Console.WriteLine($"{item.Value.Name} {item.Value.SideLength} " +
+                    $"{item.Value.SideLength} {item.Value.Figure1}");
+            }
+
+
+
+
+
+
         }
     }
 }
